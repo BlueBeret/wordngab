30415bc3b3a243e9fc2993c578fe6ca8fbe20a65
BlueBeret <your_email@example.com>
2022-02-01 22:54:01 +0700

    can read keboard

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ require('chromedriver');
 const { WebElement } = require('selenium-webdriver');
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 async function test(){
     let driver = await new Builder().forBrowser('chrome').build();
 
@@ -10,18 +14,39 @@ async function test(){
         await driver.get("https://www.powerlanguage.co.uk/wordle/");
         console.log("loaded")
         await driver.findElement(By.xpath('/html/body')).click()
-        await driver.findElement(By.xpath('/html/body')).sendKeys("grape",Key.RETURN)
+        await driver.findElement(By.xpath('/html/body')).sendKeys("tried",Key.RETURN)
 
-        let gameapp = await driver.findElement(By.tagName('game-app')); //shadow host root element
-        // create a javascript executor
-        let jse = driver.getExecutor();
-        console.log(buttonA.getText())
+        let keyboard = await driver.executeScript('return document.querySelector("body > game-app").shadowRoot.querySelector("#game > game-keyboard").shadowRoot.querySelector("#keyboard")')
+
+        let letters = await keyboard.findElements(By.tagName('button'))
+
+        let corrects = []
+        let absents = []
+        let presents = []
+
+        await sleep("2000")
+        for( const letter of letters){
+            switch (await letter.getAttribute('data-state')) {
+                case 'correct':
+                    corrects.push(await letter.getText())
+                    break;
+                case 'absent':
+                    absents.push(await letter.getText())
+                    break;
+                case 'present':
+                    presents.push(await letter.getText())
+                    break;
+                default:
+                    break;
+            }
+        }
 
     } catch (error) {
         console.log(error);
     } finally {
         console.log("Test finished");
     } 
+    driver.close()
 }
 
 test()
