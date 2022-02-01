aa9b2b00ff58e232ba1d9500217735a8a89ed32e
BlueBeret <your_email@example.com>
2022-02-01 22:01:39 +0700

    done with input handle

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("nice one")
+require('chromedriver');
+
+const { WebElement } = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+async function test(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+        await driver.get("https://www.powerlanguage.co.uk/wordle/");
+        console.log("loaded")
+        await driver.findElement(By.xpath('/html/body')).click()
+        await driver.findElement(By.xpath('/html/body')).sendKeys("grape",Key.RETURN)
+
+        let gameapp = await driver.findElement(By.tagName('game-app')); //shadow host root element
+        // create a javascript executor
+        let jse = driver.getExecutor();
+        console.log(buttonA.getText())
+
+    } catch (error) {
+        console.log(error);
+    } finally {
+        console.log("Test finished");
+    } 
+}
+
+test()
