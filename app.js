51628500d0958b36511f8ba0df44624d1e02ce36
BlueBeret <your_email@example.com>
2022-02-01 23:12:07 +0700

    encapsulate some task in function

--- app.js
+++ app.js
@@ -2,51 +2,74 @@ require('chromedriver');
 
 const { WebElement } = require('selenium-webdriver');
 const {Builder, By, Key, until} = require('selenium-webdriver');
+const readline = require('readline');
+
+function input(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }))
+}
+
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-async function test(){
-    let driver = await new Builder().forBrowser('chrome').build();
+function guess(driver, word){
+    driver.findElement(By.xpath('/html/body')).sendKeys(word,Key.RETURN)
+}
+
+async function getState(driver){
+    let corrects = []
+    let absents = []
+    let presents = []
+
+    let keyboard = await driver.executeScript('return document.querySelector("body > game-app").shadowRoot.querySelector("#game > game-keyboard").shadowRoot.querySelector("#keyboard")')
+    let letters = await keyboard.findElements(By.tagName('button'))
+    await sleep("2000")
+    for( const letter of letters){
+        switch (await letter.getAttribute('data-state')) {
+            case 'correct':
+                corrects.push(await letter.getText())
+                break;
+            case 'absent':
+                absents.push(await letter.getText())
+                break;
+            case 'present':
+                presents.push(await letter.getText())
+                break;
+            default:
+                break;
+        }
+    }
+
+    return {corrects, absents, presents}
+}
+
+async function main(){
 
     try {
+        var driver = await new Builder().forBrowser('chrome').build();
+
         await driver.get("https://www.powerlanguage.co.uk/wordle/");
-        console.log("loaded")
         await driver.findElement(By.xpath('/html/body')).click()
-        await driver.findElement(By.xpath('/html/body')).sendKeys("tried",Key.RETURN)
+        
+        await guess(driver, "grape")
 
-        let keyboard = await driver.executeScript('return document.querySelector("body > game-app").shadowRoot.querySelector("#game > game-keyboard").shadowRoot.querySelector("#keyboard")')
+        let {corrects, absents, presents} = await getState(driver)
+        console.log(corrects, absents, presents)
 
-        let letters = await keyboard.findElements(By.tagName('button'))
-
-        let corrects = []
-        let absents = []
-        let presents = []
-
-        await sleep("2000")
-        for( const letter of letters){
-            switch (await letter.getAttribute('data-state')) {
-                case 'correct':
-                    corrects.push(await letter.getText())
-                    break;
-                case 'absent':
-                    absents.push(await letter.getText())
-                    break;
-                case 'present':
-                    presents.push(await letter.getText())
-                    break;
-                default:
-                    break;
-            }
-        }
-
-    } catch (error) {
-        console.log(error);
     } finally {
         console.log("Test finished");
     } 
+    const ans = await input("Enter to quit")
     driver.close()
 }
 
-test()
+main()
